86c47a5844ec15275009cfa0c6382fef2bf335b7
Kenneth <kdarter3@gatech.edu>
2022-05-06 15:29:19 -0400

    may 6 update

--- README.md
+++ README.md
@@ -1,14 +1,39 @@
 # call_center_simulation
 
 ## TO DO
-- [X] output takes labels from tier values
-- [ ] generalize labels and comments to remove MSCC language
+- [X] generalize labels and comments to remove MSCC language
 - [ ] get rid of pandas error message
 - [ ] create runs to compare multiple scenarios
 - [ ] add graphic output with histograms
 - [ ] put in jupyter notebook for users to run
 - [ ] publish as example for simpy
-- [ ] check program from work laptop for changes (initial calls?)
-- [ ] trim variables being passed into functions
+- [ ] comment all code
+- [ ] put descriptions in new readme for program
+- [X] check program from work laptop for changes (initial calls?)
+- [X] trim variables being passed into functions
 - [X] put functions into class except for run simulation
 - [X] make print output part of run simulation function
+- [X] output takes labels from tier values
+
+
+Call Center Simulation
+
+Scenario:
+  The Call Center has a limited number of agents and defines
+  a call processes that takes some (random) time.
+
+  Callers arrive at the Call Center at a random time. If one
+  agent is available, they start the call process and wait for it
+  to finish. If not, the caller waits until agent is available.
+
+The Call Center has a limited number of agents to answer 
+    calls in parallel
+
+    Callers have to request one of the agents. When they got one, they
+    can start the call processes and wait for it to finish.
+
+The call process - caller arrives at the CC
+    and requests an agent.
+
+    It then starts the call process, waits for it to finish and
+    leaves.
--- simulation.py
+++ simulation.py
@@ -1,38 +1,19 @@
-"""
-MSCC Simulation
-
-Scenario:
-  The MSCC has a limited number of CSRs and defines
-  a call processes that takes some (random) time.
-
-  Callers arrive at the MSCC at a random time. If one CSR
-  is available, they start the call process and wait for it
-  to finish. If not, they wait until CSR is available.
-
-"""
 import random
 import pandas as pd
 import simpy
 import numpy as np
 
 
-class MSCC(object):
-    """The MSCC has a limited number of CSRs to answer calls in parallel
-
-    Callers have to request one of the CSRs. When they got one, they
-    can start the call processes and ans for it to finish (which
-    takes ``talk_time`` minutes).
-
-    """
-    def __init__(self, env, num_csrs):
+class CC(object):
+    def __init__(self, env, num_agents):
         self.env = env
-        self.csrs = []
+        self.agents = []
 
-        for num in num_csrs:
-            self.csrs.append(simpy.Resource(env,num))
+        for num in num_agents:
+            self.agents.append(simpy.Resource(env,num))
 
     def call(self, tier,df,tier_params):
-        """The call processes. It takes a ``caller`` processes and answers it."""
+        """The call processes. It takes a ``caller`` process and answers it."""
         talk_time = np.random.normal(tier_params['Talk_mu'][tier],tier_params['Talk_std'][tier])
         if talk_time < 0:
             talk_time = 0
@@ -53,15 +34,7 @@ class MSCC(object):
             df['Transfer'][tier] += 1
         yield self.env.timeout(call_time)
 
-    def caller(self, tier, mscc,df,svc_lvl_thresh,tier_params,num_tiers):
-        """The call process - caller arrives at the MSCC
-        and requests a CSR.
-
-        It then starts the call process, waits for it to finish and
-        leaves.
-
-        """
-
+    def caller(self, tier, cc,df,svc_lvl_thresh,tier_params,num_tiers):
         start = self.env.now
         patience = np.random.normal(tier_params['Patience'][tier],60)
         if patience < 1:
@@ -71,11 +44,11 @@ class MSCC(object):
         choice = tier
 
         for count in range(num_tiers-1,tier-1,-1):
-            if (len(self.csrs[count].queue)==0):
+            if (len(self.agents[count].queue)==0):
                 choice = count
 
             
-        with self.csrs[choice].request() as request:
+        with self.agents[choice].request() as request:
             results = yield request | self.env.timeout(patience)
             df['Offered'][tier] += 1
             end = self.env.now
@@ -84,17 +57,16 @@ class MSCC(object):
                 df['Ans_Time'][tier] += (end-start)
                 if (end-start) <= svc_lvl_thresh:
                     df['Within_Svc_Lvl'][tier] += 1
-                yield self.env.process(mscc.call(tier,df,tier_params))
+                yield self.env.process(cc.call(tier,df,tier_params))
             else:
                 df['Abandon'][tier] += 1
     
-    def setup(self, mscc,num_csrs,t,df,svc_lvl_thresh,tier_params,num_tiers):
-        """Create MSCC, a number of initial callers and keep creating callers
+    def setup(self, cc,t,df,svc_lvl_thresh,tier_params,num_tiers):
+        """Create CC, a number of initial callers and keep creating callers
         approx. every ``t`` seconds."""
-        #mscc_sim = MSCC(self.env, num_csrs)
 
-        total_csrs = tier_params['CSRs'].sum()
-        initial_calls = round(total_csrs*.95)
+        total_agents = tier_params['Agents'].sum()
+        initial_calls = round(total_agents*.95)
         i = 0
         for i in range(initial_calls):
             tier_rand = random.random()
@@ -105,7 +77,7 @@ class MSCC(object):
                     tier = count
             df['Entered'][tier] += 1
 
-            self.env.process(mscc.caller(tier, mscc,df,svc_lvl_thresh,tier_params,num_tiers))
+            self.env.process(cc.caller(tier, cc,df,svc_lvl_thresh,tier_params,num_tiers))
             i += 1
 
 
@@ -121,12 +93,12 @@ class MSCC(object):
                     tier = count
             df['Entered'][tier] += 1
 
-            self.env.process(mscc.caller(tier, mscc,df,svc_lvl_thresh,tier_params,num_tiers))
+            self.env.process(cc.caller(tier, cc,df,svc_lvl_thresh,tier_params,num_tiers))
 
 
 def run_Simulation(sim_params,tier_params):
 
-    Num_CSRs = tier_params['CSRs']
+    num_agents = tier_params['Agents']
 
 
     T_INTER = sim_params['Arrivals'][0]
@@ -134,7 +106,8 @@ def run_Simulation(sim_params,tier_params):
     svc_lvl_thresh = sim_params['svc_lvl_thresh'][0]
     NUM_SIMS = sim_params['NUM_SIMS'][0]
     i = 0
-    master = {'Entered':[],'Offered':[],'Handled':[],'Talk_Time':[],'Ans_Time':[],'Hold_Time':[],'Wrap_Time':[],'Transfer':[],'Abandon':[],'Within_Svc_Lvl':[],'Tier':[],'Iteration':[]}
+    master = {'Entered':[],'Offered':[],'Handled':[],'Talk_Time':[],'Ans_Time':[],'Hold_Time':[],'Wrap_Time':[],\
+        'Transfer':[],'Abandon':[],'Within_Svc_Lvl':[],'Tier':[],'Iteration':[]}
     master_df = pd.DataFrame(master)
     num_tiers = (len(tier_params))
     s_val = [0] * num_tiers
@@ -144,11 +117,12 @@ def run_Simulation(sim_params,tier_params):
 
     for i in range(NUM_SIMS):
         iters = [i] * num_tiers
-        d = {'Entered':s_val,'Offered':s_val,'Handled':s_val,'Talk_Time':s_val,'Ans_Time':s_val,'Hold_Time':s_val,'Wrap_Time':s_val,'Transfer':s_val,'Abandon':s_val,'Within_Svc_Lvl':s_val,'Tier':tiers,'Iteration':iters}
+        d = {'Entered':s_val,'Offered':s_val,'Handled':s_val,'Talk_Time':s_val,'Ans_Time':s_val,'Hold_Time':s_val,'Wrap_Time':\
+            s_val,'Transfer':s_val,'Abandon':s_val,'Within_Svc_Lvl':s_val,'Tier':tiers,'Iteration':iters}
         df = pd.DataFrame(data=d)
         env = simpy.Environment()
-        mscc_sim = MSCC(env, Num_CSRs)
-        env.process(mscc_sim.setup(mscc_sim,Num_CSRs, T_INTER,df,svc_lvl_thresh,tier_params,num_tiers))
+        mscc_sim = CC(env, num_agents)
+        env.process(mscc_sim.setup(mscc_sim,T_INTER,df,svc_lvl_thresh,tier_params,num_tiers))
         env.run(until=SIM_TIME)
         master_df = pd.concat([master_df,df],ignore_index=True)
         i += 1
@@ -162,9 +136,10 @@ def run_Simulation(sim_params,tier_params):
 
     return master_df
 
-params_dict = {'Arrivals':1,'SIM_TIME':600,'svc_lvl_thresh':30,'NUM_SIMS':1}
-sim_params = pd.DataFrame(params_dict,index=[0])
-tier_params_dict = {'Tier':['T1','T2','T3'],'Talk_mu':[397,487,396],'Wrap_mu':[62,95,79],'Hold_mu':[83,97,87],'Talk_std':[60,79,90],'Wrap_std':[20,40,47],'Hold_std':[20,34,52],'Volume':[0.7,0.97,1],'Patience':[320,480,240],'Transfer':[0.3,0.22,0.34],'CSRs':[321,94,9]}
+sim_dict = {'Arrivals':1,'SIM_TIME':600,'svc_lvl_thresh':30,'NUM_SIMS':1}
+sim_params = pd.DataFrame(sim_dict,index=[0])
+tier_params_dict = {'Tier':['T1','T2','T3'],'Talk_mu':[397,487,396],'Wrap_mu':[62,95,79],'Hold_mu':[83,97,87],'Talk_std':[60,79,90],\
+    'Wrap_std':[20,40,47],'Hold_std':[20,34,52],'Volume':[0.7,0.97,1],'Patience':[320,480,240],'Transfer':[0.3,0.22,0.34],'Agents':[321,94,9]}
 tier_params = pd.DataFrame(tier_params_dict)
 
 results = run_Simulation(sim_params,tier_params)
